6dec8b75be3c91a911e0f12c9fa9ca0f408bd951
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-05-28 00:56:43 -0400

    compare cards and able to bet

--- js/main.js
+++ js/main.js
@@ -3,39 +3,48 @@ const player = 1
 const computerPlayer = -1
 const SUIT = ['d', 'h', 's', 'c']
 const NUM = [
-  'A',
-  '02',
-  '03',
-  '04',
-  '05',
-  '06',
-  '07',
-  '08',
-  '09',
-  '10',
-  'J',
-  'Q',
-  'K',
+    'A',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    'J',
+    'Q',
+    'K',
 ]
 const PLAYER = '#player-card'
 const COMPUTER = '#computer-card'
 
 /*----- app's state (variables) -----*/
-computerPoint = 5
-playerPoint = 5
+computerPoint = 5;
+playerPoint = 5;
 
-playerCard = document.querySelector('#player-card')
-computerCard = document.querySelector('#computer-card')
+//playerchoice large/small and even/odd
+playerChoice = null;
+
+
 
 /*----- cached element references -----*/
+let playerCard = document.querySelector('#player-card');
+let computerCard = document.querySelector('#computer-card');
+
+
+
 
 /*----- event listeners -----*/
 
+//click playercard and create a card
 playerCard.addEventListener('click', function () {
-  createCard(PLAYER)
+    createCard(PLAYER);
 })
 
-// 'click' playercard run createCard
+let betButton = document.querySelector('#bet-button')
+betButton.addEventListener('click',bet);
 
 //click on bet button
 // if (large or small && odd or even not clicked){
@@ -49,81 +58,119 @@ playerCard.addEventListener('click', function () {
 /*----- functions -----*/
 
 function initialize() {
-  //      COMPUTER = classlist back-red;
-  computerCard.classList.add('back-red')
+    //      COMPUTER = classlist back-red;
+    computerCard.classList.add('back-red');
 
-  //      PLAYER = classlist back-blue;
-  playerCard.classList.add('back-blue')
-  //      id display = "Choose your bet and click on your bet"
-  let display = document.querySelector('#display')
-  display.innerText = 'Check your card first and then choose bet'
-  //      computer point = 5;
-  displayComPoint = document.querySelector('#computer-point')
-  displayComPoint.innerText = 'Computer points: ' + computerPoint
-
-  //      player point = 5;
-  displayComPoint = document.querySelector('#player-point')
-  displayComPoint.innerText = 'Player points: ' + playerPoint
+    //      PLAYER = classlist back-blue;
+    playerCard.classList.add('back-blue');
+    //      id display = "Choose your bet and click on your bet"
+    display = document.querySelector('#display');
+    display.innerText = 'Check your card first and then choose bet';
+    //      computer point = 5;
+    displayComPoint = document.querySelector('#computer-point');
+    
+    displayComPoint.innerText = 'Computer points: ' + computerPoint;
+    //      player point = 5;
+    displayPlayerPoint = document.querySelector('#player-point');
+    displayPlayerPoint.innerText = 'Player points: ' + playerPoint;
 }
 
 initialize()
 
 //Create Card for computer and player
 getSuit = function () {
-  i = Math.floor(Math.random() * SUIT.length)
-  return SUIT[i]
+    i = Math.floor(Math.random() * SUIT.length)
+    return SUIT[i]
 }
 
 getNum = function () {
-  i = Math.floor(Math.random() * NUM.length)
-  return NUM[i]
+    i = Math.floor(Math.random() * NUM.length)
+    return NUM[i]
 }
 
 function createCard(id) {
-  let element = document.querySelector(id)
-  if (id == '#player-card' && element.classList.contains('back-blue')) {
-    element.classList.remove('back-blue');
-    element.classList.add(getSuit() + getNum())
-  } else if (id == '#player-card' && element.classList.contains('back-red')){
-    element.classList.remove('back-red');
-    element.classList.add(getSuit() + getNum())
-  }
+    let element = document.querySelector(id)
+    if (id == '#player-card' && element.classList.contains('back-blue')) {
+        element.classList.remove('back-blue');
+        element.classList.add(getSuit() + getNum())
+    } else if (id == '#computer-card' && element.classList.contains('back-red')) {
+        element.classList.remove('back-red');
+        element.classList.add(getSuit() + getNum())
+    }
 }
 
-//compare Card
 
-// compareLargeSmall(){
-//     if (checked large){
-//         if (playercard > computercard){
-//             playerpoint += 1;
-//         }else {
-//             playerpoint -= 1;
-//         }
+//find player choice
+function findPlayerChoice() {
+    let radio = document.getElementsByName('opt');
+    for (var i = 0, length = radio.length; i < length; i++) {
+        if (radio[i].checked) {
+            // do whatever you want with the checked radio
+            playerChoice = radio[i].value;
+            // only one radio can be logically checked, don't check the rest
+            break;
+        }
+    }
+}
 
-//     } if (checked small){
-//     if (playercard < computercard){
-//         playerpoint += 1;
-//     }else {
-//         playerpoint -= 1;
-//     }
+//compare card between player and computer
 
-// }
-// compareEvenOdd(){
-//     if (checked even){
-//         if computercard = even{
-//             player += 1
-//         }else{
-//             player -= 1;
-//         }
-//     }
-//     if (player checked odd) {
-//         if (computerCard = odd){
-//             playerpoint +=1
-//         }else{
-//             playepoint-+1
-//         }
-//     }
-// }
+function playerGain(point){
+    playerPoint += point;
+    computerPoint -=point;
+    if (point>0){
+        display.innerText = 'You gain:' + point + ' point(s)';
+    }
+    if (point<0){
+        display.innerText = 'You lose:' + point*('-1') + ' point(s)'
+    }
+}
+
+
+function compareCard(){
+    numOfPlayerCard = playerCard.classList[1].substring(1);
+    playerCardNum = NUM.indexOf(numOfPlayerCard);
+    numOfComputerCard = computerCard.classList[1].substring(1);
+    computerCardNum = NUM.indexOf(numOfComputerCard);
+
+    if (playerChoice == 'large') {
+        if (playerCardNum > computerCardNum) {
+            playerGain(1)
+            
+        } else {
+            playerGain('-1')
+        }
+
+    }
+    if (playerChoice == 'small') {
+        if (playerCardNum < computerCardNum) {
+           playerGain(1)
+        } else {
+            playerGain(1)
+        }
+    }
+    if (playerChoice == 'equal') {
+        if (playerCardNum = computerCardNum) {
+            playerGain(2)
+        } else {
+            playerGain('-2')
+        }
+    }
+    
+}
+
+//after click bet 
+function bet() {
+    findPlayerChoice();
+    console.log(playerChoice);
+    if(playerCard.classList.contains('back-blue')|| playerChoice == null){
+        return;
+    }
+    else {createCard(COMPUTER);
+        compareCard();
+    }
+}
+
 
 //check win
 // checkWin(){
