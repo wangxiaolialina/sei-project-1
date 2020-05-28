2f458106bfb8bbb51eb53e65107a0f818e1b7bea
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-05-28 15:26:32 -0400

    reset button complete

--- js/main.js
+++ js/main.js
@@ -3,57 +3,61 @@ const player = 1
 const computerPlayer = -1
 const SUIT = ['d', 'h', 's', 'c']
 const NUM = [
-    'A',
-    '02',
-    '03',
-    '04',
-    '05',
-    '06',
-    '07',
-    '08',
-    '09',
-    '10',
-    'J',
-    'Q',
-    'K',
+  'A',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  'J',
+  'Q',
+  'K',
 ]
 const PLAYER = '#player-card'
 const COMPUTER = '#computer-card'
+const INITIALPOINT = 5;
+const WINPOINT = 8;
 
 /*----- app's state (variables) -----*/
-computerPoint = 5;
-playerPoint = 5;
+
+
+let computerPoint = INITIALPOINT;
+let playerPoint = INITIALPOINT;
 
 //playerchoice large/small and even/odd
-playerChoice = null;
-
-
+playerChoice = null
 
 /*----- cached element references -----*/
 let playerCard = document.querySelector('#player-card');
 let computerCard = document.querySelector('#computer-card');
-let nextRoundButton=document.createElement('button');
-
-
-
+let nextRoundButton = document.createElement('button');
+let displayComPoint = document.querySelector('#computer-point');
+let displayPlayerPoint = document.querySelector('#player-point');
+let display = document.querySelector('#display');
+let replayButton = document.querySelector('#reset');
 
 /*----- event listeners -----*/
 
 //click playercard and create a card
 playerCard.addEventListener('click', function () {
-    createCard(PLAYER);
+  createCard(PLAYER);
 })
 
 let betButton = document.querySelector('#bet-button')
-betButton.addEventListener('click',bet);
+betButton.addEventListener('click', bet)
 
-
-nextRoundButton.addEventListener('click',function(){
-    document.querySelector('input[name="opt"]:checked').checked = false;
-    playerCard.classList.remove(playerCard.classList[1]);
-    computerCard.classList.remove(computerCard.classList[1]);
-    initialize();
+nextRoundButton.addEventListener('click', function () {
+  document.querySelector('input[name="opt"]:checked').checked = false;
+  playerCard.classList.remove(playerCard.classList[1]);
+  computerCard.classList.remove(computerCard.classList[1]);
+  initialize()
 })
+
+replayButton.addEventListener('click', reset);
 
 //click on bet button
 // if (large or small && odd or even not clicked){
@@ -67,141 +71,115 @@ nextRoundButton.addEventListener('click',function(){
 /*----- functions -----*/
 
 function initialize() {
-    //      COMPUTER = classlist back-red;
-    computerCard.classList.add('back-red');
+  //      COMPUTER = classlist back-red;
+  computerCard.classList.add('back-red')
 
-    //      PLAYER = classlist back-blue;
-    playerCard.classList.add('back-blue');
-    //      id display = "Choose your bet and click on your bet"
-    display = document.querySelector('#display');
-    display.innerText = 'Check your card first and then choose bet';
-    //      computer point = 5;
-    displayComPoint = document.querySelector('#computer-point');
-    
-    displayComPoint.innerText = 'Computer points: ' + computerPoint;
-    //      player point = 5;
-    displayPlayerPoint = document.querySelector('#player-point');
-    displayPlayerPoint.innerText = 'Player points: ' + playerPoint;
-    //remove classList
+  //      PLAYER = classlist back-blue;
+  playerCard.classList.add('back-blue')
+  //      id display = "Choose your bet and click on your bet"
+  display = document.querySelector('#display')
+  display.innerText = 'Check your card first and then choose bet'
+  //      computer point = 5;
 
+  displayComPoint.innerText = 'Computer points: ' + computerPoint
+  //      player point = 5;
+  displayPlayerPoint.innerText = 'Player points: ' + playerPoint
 }
 
 initialize()
 
 //Create Card for computer and player
 getSuit = function () {
-    i = Math.floor(Math.random() * SUIT.length)
-    return SUIT[i];
+  i = Math.floor(Math.random() * SUIT.length)
+  return SUIT[i]
 }
 
 getNum = function () {
-    i = Math.floor(Math.random() * NUM.length)
-    return NUM[i];
+  i = Math.floor(Math.random() * NUM.length)
+  return NUM[i]
 }
 
 function createCard(id) {
-    let element = document.querySelector(id)
-    if (id == '#player-card' && element.classList.contains('back-blue')) {
-        element.classList.remove('back-blue');
-        element.classList.add(getSuit() + getNum())
-    } else if (id == '#computer-card' && element.classList.contains('back-red')) {
-        element.classList.remove('back-red');
-        element.classList.add(getSuit() + getNum())
-    }
+  let element = document.querySelector(id)
+  if (id == '#player-card' && element.classList.contains('back-blue')) {
+    element.classList.remove('back-blue')
+    element.classList.add(getSuit() + getNum())
+  } else if (id == '#computer-card' && element.classList.contains('back-red')) {
+    element.classList.remove('back-red')
+    element.classList.add(getSuit() + getNum())
+  }
 }
-
 
 //find player choice
 function findPlayerChoice() {
-    let radio = document.getElementsByName('opt');
-    for (var i = 0, length = radio.length; i < length; i++) {
-        if (radio[i].checked) {
-            // do whatever you want with the checked radio
-            playerChoice = radio[i].value;
-            // only one radio can be logically checked, don't check the rest
-            break;
-        }
+  let radio = document.getElementsByName('opt')
+  for (var i = 0, length = radio.length; i < length; i++) {
+    if (radio[i].checked) {
+      // do whatever you want with the checked radio
+      playerChoice = radio[i].value
+      // only one radio can be logically checked, don't check the rest
+      break
     }
+  }
 }
 
 //compare card between player and computer
 
-function playerGain(point){
-    playerPoint += point;
-    computerPoint -=point;
-    if (point>0){
-        display.innerText = 'You gain:' + point + ' point(s)';
+function compareCard() {
+  numOfPlayerCard = playerCard.classList[1].substring(1)
+  playerCardNum = NUM.indexOf(numOfPlayerCard)
+  numOfComputerCard = computerCard.classList[1].substring(1)
+  computerCardNum = NUM.indexOf(numOfComputerCard)
+
+  if (playerChoice == 'large') {
+    if (playerCardNum > computerCardNum) {
+      displayMessage(1)
+    } else {
+      displayMessage(-1)
     }
-    if (point<0){
-        display.innerText = 'You lose:' + point*(-1) + ' point(s)'
+  } else if (playerChoice == 'small') {
+    if (playerCardNum < computerCardNum) {
+      displayMessage(1)
+    } else {
+      displayMessage(-1)
     }
-    
-}
-
-
-function compareCard(){
-    numOfPlayerCard = playerCard.classList[1].substring(1);
-    playerCardNum = NUM.indexOf(numOfPlayerCard);
-    numOfComputerCard = computerCard.classList[1].substring(1);
-    computerCardNum = NUM.indexOf(numOfComputerCard);
-
-    if (playerChoice == 'large') {
-        if (playerCardNum > computerCardNum) {
-            playerGain(1)
-            
-        } else {
-            playerGain(-1)
-        }
-
-    } else if (playerChoice == 'small') {
-        if (playerCardNum < computerCardNum) {
-           playerGain(1)
-        } else {
-            playerGain(-1)
-        }
-    } else if (playerChoice == 'equal') {
-        if (playerCardNum == computerCardNum) {
-            playerGain(2)
-        } else {
-            playerGain(-2)
-        }
+  } else if (playerChoice == 'equal') {
+    if (playerCardNum == computerCardNum) {
+      displayMessage(2)
+    } else {
+      displayMessage(-2)
     }
-    displayPlayerPoint.innerText = 'Player points: ' + playerPoint;
-    displayComPoint.innerText = 'Computer points: ' + computerPoint;
+  }
 }
 
 //show Next round button
-function nextRound (){
-    nextRoundButton.innerText='Next Round';
-    document.getElementById("display").appendChild(nextRoundButton);
+function nextRound() {
+  nextRoundButton.innerText = 'Next Round'
+  document.getElementById('display').appendChild(nextRoundButton)
 }
-//after click bet 
-function bet() {
-    findPlayerChoice();
-    console.log(playerChoice);
-    if(playerCard.classList.contains('back-blue')|| playerChoice == null){
-        return;
-    }
-    else {createCard(COMPUTER);
-        compareCard();
-        nextRound();
-    }
-}
-
 
 //check win
-// checkWin(){
-//     if (playerPoints=10) {
-//         display.innerText  =  "You are the Winner!"
-//         // display.style.'font-size' = 2vw;
-//     reset()
-//     }
-//     if (computerPoints = 10) {
-//         display.innerText = "Sorry, try again"
-//         // display.style.'font-size' = 2vw;
-//         reset()
-//     }
-// }
+
+function displayMessage(point) {
+  playerPoint = playerPoint + point;
+  computerPoint = computerPoint - point;
+  if (playerPoint < WINPOINT && computerPoint < WINPOINT) {
+    if (point > 0) {
+      display.innerText = 'You gain:' + point + ' point(s)'
+    }
+    if (point < 0) {
+      display.innerText = 'You lose:' + point * (-1) + ' point(s)'
+    }
+    nextRound();
+  } else if (playerPoint == WINPOINT) {
+    display.innerText = display.innerText = 'You are the Winner!'
+  } else if(computerPoint == WINPOINT) {
+    display.innerText = display.innerText = 'Sorry, try again'
+  }
+  displayPlayerPoint.innerText = 'Player points: ' + playerPoint;
+  displayComPoint.innerText = 'Computer points: ' + computerPoint;
+}
+
 
 //reset()
 // COMPUTER = classlist back-red;
@@ -209,3 +187,34 @@ function bet() {
 //      id display = "Choose your bet and click on your bet"
 //      computer point = 5;
 //      player point = 5;
+
+//after click bet
+function bet() {
+    findPlayerChoice();
+  if (playerCard.classList.contains('back-blue') || playerChoice == null) {
+    return
+  } else {
+    createCard(COMPUTER);
+    compareCard();
+  }
+}
+
+//reset()
+function reset (){
+  //      COMPUTER = classlist back-red;
+  computerCard.removeAttribute('class');
+  computerCard.setAttribute('class','card back-red');
+
+  //      PLAYER = classlist back-blue;
+  playerCard.removeAttribute('class');
+  playerCard.setAttribute('class','card back-blue');
+  //      id display = "Choose your bet and click on your bet"
+  display.innerText = 'Check your card first and then choose bet'
+//clear radio choice
+  document.querySelector('input[name="opt"]:checked').checked = false;
+  //      computer point = 5;
+  displayComPoint.innerText = 'Computer points: ' + INITIALPOINT;
+  //      player point = 5;
+  displayPlayerPoint.innerText = 'Player points: ' + INITIALPOINT;
+}
+
